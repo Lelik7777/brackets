8172f7d8b717e7ffe259b12e6befeb4723cbb7b2
Lelik Jan <21alex7777@gmail.com>
2023-02-07 20:07:41 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    const stack = [];
+    //get object with key=')' and value='(' and etc
+    const objBrackets = bracketsConfig.reduce((acc, cur) => {
+        acc[cur[1]] = cur[0];
+        return acc;
+    }, {});
+    str.split('').forEach(el => {
+        if (stack.length===0) stack.push(el);
+        else if (stack[stack.length - 1] === objBrackets[el]) stack.pop();
+        else stack.push(el);
+    });
+    return stack.length===0;
 }
